5a00e1275e80b57dc94c291aa5c494654ae94afb
Nate Rudolph <naterudolph@gmail.com>
2013-03-02 16:50:05 -0500

    eyeBall Prototype
    arduino sketch, ATtiny85 wiring diagram

---
+++ eyeBallPrototype.ino
@@ -0,0 +1,35 @@
+/* 3/1/13
+   eyeBall prototype
+   (insert link to images)
+
+    Wiring Diagram for atTiny85 in eyeBall
+     
+                         ______
+               Reset  - |      | - VCC (+) ***POWER IN***
+***PHOTO***    Pin 3  - |      | - Pin 2
+               Pin 4  - |      | - Pin 1
+***GROUND*** (-) GND  - |______| - Pin 0  ***LED***
+
+
+nateRudolph@gmail.com   
+*/
+
+void setup()
+{
+ pinMode(0, OUTPUT);
+ pinMode(3, INPUT);
+ digitalWrite(3, HIGH);
+ digitalWrite(0, HIGH); 
+}
+
+void loop()
+{
+ int a = analogRead(3);
+ 
+  if(a > 80){
+   digitalWrite(0, HIGH); 
+  }
+  else{
+   digitalWrite(0, LOW); 
+  }
+}
